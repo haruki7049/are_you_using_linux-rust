73e5d570ba748a002018246f78ae91c708b1d243
haruki7049 <tontonkirikiri@gmail.com>
2022-12-23 10:16:16 +0900

    added lib

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod lib{
+    pub fn are_you_using_linux(){
+        if cfg!(target_os = "linux"){
+            println!("You are using Linux!! You are My friends!! Foo!!");
+        }else if cfg!(target_os = "macos"){
+            println!("You are using Mac OS. Surely Mac OS is cool. I understood why you are using Mac OS.");
+        }else if cfg!(target_os = "windows"){
+            println!("You are using Windows. Why don't you use Linux? The OS give you very nice experience!!");
+        }else{
+            println!("Wow!! You are using the OS which is not known by us!! You should be using the OS!!");
+        }
+    }
+}
